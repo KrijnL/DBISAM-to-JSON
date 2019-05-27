f09d70b15f3c78075ab4150799df15b0d8592100
KrijnLuijendijk-r0666692 <krijn.luijendijk@student.ucll.be>
2019-05-27 18:18:26 +0200

    forgot to add python3 col_types

---
+++ python3/col_types.py
@@ -0,0 +1,6 @@
+types = {
+    b'\x02': {'col_type': 'date', 'col_length': 4},
+    b'\x06': {'col_type': 'int', 'col_length': 4},
+    b'\x07': {'col_type': 'float', 'col_length': 8},
+    b'\x01': {'col_type': 'string', 'col_length': 0}
+}
